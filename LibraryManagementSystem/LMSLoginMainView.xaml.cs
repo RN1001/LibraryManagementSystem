373063be7a306ad350be21be30d53b674862cd40
M Nazir <Aeth1001@hotmail.com>
2017-11-19 19:01:41 +0000

    #UIDesign, moved Mainwindow.xaml into views, added code to log into the application through the log in form.

---
+++ LibraryManagementSystem/LMSLoginMainView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for LMSLoginView.xaml
+    /// </summary>
+    public partial class LMSLoginView : Window
+    {
+        public LMSLoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+
+            if (UsernameTbx.Text == "admin" && PasswordTbx.Password == "password")
+            {
+                mw.Show();
+                this.Close();
+            }
+            else
+            {
+                UsernameTbx.Clear();
+                PasswordTbx.Clear();
+                ErrorLabel.Content = "Error occured, please try again.";
+            }
+
+            
+        }
+
+         
+    }
+}
